139cbdbb18f35bc84f7375134ee8be36599cf8a8
mohamed saleh <mohamed20163858@gmail.com>
2024-02-01 23:03:16 +0200

    Test pusher in front end

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import { useEffect } from "react";
 import "./App.css";
 import Chat from "./Chat";
 import Sidebar from "./Sidebar";
+import Pusher from "pusher-js";
 
 function App() {
+  useEffect(() => {
+    const  pusher = new Pusher('b0b14468ad9da69f01ae', {
+      cluster: 'eu'
+    });
+
+    const channel = pusher.subscribe('messages');
+    channel.bind('inserted', function(data) {
+      alert(JSON.stringify(data));
+    });
+  }, [])
   return (
     <div className="app">
       <div className="app_body">
